0cd078525af034538b322ce512d9585d2bdf44c1
Izzet Eray Ertoprak <izzeteray@googlemail.com>
2024-05-23 17:36:10 +0300

    Update main.dart
    centered the text on appbar
    changed the background color
    
    Added an image on home

--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Center(child:Text('Hello world'),),),);
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        appBar: AppBar(
+          title: Center(
+            child: Text(Colors.white'I am new'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUcDnQhEkAOVt2kHZNu-IdRz4y-4y_EzGDySGy7tK3yQ&s'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
